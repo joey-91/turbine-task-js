b46308d7dcb9c11901a914da4311b2cc8f781166
Joseph Stern <47149663+joey-91@users.noreply.github.com>
2024-11-05 01:33:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,30 +9,37 @@ from src.schemas import bronze_schema, silver_schema, gold_schema, gold_statisti
 ENGINE = create_db(db_name='turbine_data.db')
 
 def bronze(engine):
-    raw_df = concat_files(filepath='./data')
-    print(raw_df.head())
 
-    # Create Bronze table
+    # Read Raw Data
+    raw_df = concat_files(filepath='./data')
+
+    # Load to Bronze table
     load_to_db(df=raw_df, engine=engine, table_name='bronze_turbines', schema=bronze_schema)
     return
 
 def silver(engine):
+
+    # Read Bronze Data
     bronze_df = pd.read_sql('SELECT * FROM bronze_turbines', con=engine)
 
-    # Create Cleansed table
+    # Apply Cleansing Functions
     bronze_df_rn = remove_nulls(bronze_df)
     bronze_df_rn['date'] = bronze_df_rn['timestamp'].apply(extract_date)
+
+    # Load to Silver Table
     load_to_db(df=bronze_df_rn, engine=engine, table_name='silver_turbines', schema=silver_schema)
     return
 
 def gold(engine):
+
+    # Read Silver Table
     silver_df = pd.read_sql('SELECT * FROM silver_turbines', con=engine)
 
-    # Create Gold Anomaly table
+    # Apply Anomaly Calculation & Write to Gold table
     anomaly_df = anomaly_detection(df=silver_df)
     load_to_db(df=anomaly_df, engine=engine, table_name='gold_turbines', schema=gold_schema)
 
-    # Create Gold Summary table
+    # Apply Statistic Calculation & Write to Gold Table
     statistics_df = statistics_table(engine=engine, table_name='silver_turbines')
     load_to_db(df=statistics_df, engine=engine, table_name='gold_turbines_summary', schema=gold_statistics_schema)
     return
